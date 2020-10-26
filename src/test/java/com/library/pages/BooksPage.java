f0e7980feab569a1553e17693d41a371ce8b1c74
ZDagasan <zedasa80@gmail.com>
2020-10-26 11:01:04 -0500

    searchByBookCategories Done!

--- src/test/java/com/library/pages/BooksPage.java
+++ src/test/java/com/library/pages/BooksPage.java
@@ -1,8 +1,12 @@
 package com.library.pages;
 import com.library.utils.BrowserUtil;
+import com.library.utils.Driver;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.Map;
 
@@ -36,7 +40,7 @@ public class BooksPage extends BasePage{
     @FindBy (id= "description")
     private WebElement descriptionInputBox;
 
-    @FindBy(id="book_group_id")
+    @FindBy(xpath="//select[@id='book_categories']")
     private WebElement bookCategoryDropDown;
 
     @FindBy(xpath = "//button[@class='btn btn-primary']")
@@ -45,11 +49,13 @@ public class BooksPage extends BasePage{
     @FindBy(xpath = "//div[@class='toast-message']")
     private WebElement confirmationMessage;
 
+    @FindBy(xpath = "//select[@id='book_categories']/option[@value='16']")
+    private WebElement romance;
+
+
     public void click_books_tab(){
         booksTab.click();
     }
-
-
     public void search_book_by(String string){
         BrowserUtil.wait(2);
         searchInputBox.click();
@@ -58,7 +64,6 @@ public class BooksPage extends BasePage{
     public void click_addBook(){
         addBookButton.click();
     }
-
     public void addBookName(String bookName){
     BrowserUtil.enterText(bookNameInputBox,bookName);
     }
@@ -74,15 +79,21 @@ public class BooksPage extends BasePage{
     public void addDescription(String description){
         BrowserUtil.enterText(descriptionInputBox,description);
     }
-
     public void clickSaveChangesBtn(){
         clickOnElement(saveChangesBtn);
     }
-
     public boolean confirmationMessageCheck(){
         BrowserUtil.wait(2);
         return confirmationMessage.isDisplayed();
     }
+    public void clickAndSelectBookCategories(){
+        Select bookCategory = new Select(bookCategoryDropDown);
+        BrowserUtil.wait(3);
+        bookCategory.selectByVisibleText("Romance");
+    }
+
+
+
 
 
 
